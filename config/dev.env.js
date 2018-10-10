f4c743abc0b3baa2b8a03245d7732a20603b428f
Ecstack <ecstack@sina.com>
2018-10-10 17:35:18 +0800

    Update dev.env.js
    修改开发模式配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+	ENV_CONFIG: '"dev"',
+	// BASE_API: '"http://weixin.gtcx.top/"' //开发地址
+	BASE_API: '""'
 })
